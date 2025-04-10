8673a56239cd70a0b12a7a6b72af713687ccee3a
EslamKamel89 <elsamkamelforex@gmail.com>
2025-04-10 19:23:31 +0200

    feat: update nuxt.config.ts to include app head configuration and ensure proper formatting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,18 +2,33 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
 
-  modules: [
-    '@nuxt/ui',
-    '@nuxt/eslint',
-    '@nuxt/icon',
-    '@nuxt/fonts'
-  ],
+  modules: ["@nuxt/ui", "@nuxt/eslint", "@nuxt/icon", "@nuxt/fonts"],
 
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
 
   future: {
-    compatibilityVersion: 4
+    compatibilityVersion: 4,
   },
 
-  compatibilityDate: '2024-11-27'
-})
+  compatibilityDate: "2024-11-27",
+  app: {
+    head: {
+      title: "ChinaShop",
+      link: [
+        {
+          href: "https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css",
+          tagPosition: "head",
+          rel: "stylesheet",
+        },
+      ],
+      script: [
+        {
+          src: "https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js",
+          tagPosition: "bodyClose",
+          type: "text/javascript",
+          defer: true,
+        },
+      ],
+    },
+  },
+});
