507fa6246e56a2eb9f495612a5068e78ff972a21
EslamKamel89 <elsamkamelforex@gmail.com>
2025-04-18 16:55:45 +0200

    feat: implement category edit form with validation and fetch category data

--- components/Admin/Category/Form.vue
+++ components/Admin/Category/Form.vue
@@ -15,10 +15,20 @@
 </template>
 
 <script setup lang="ts">
+import { useForm } from "vee-validate";
 import Heading from "~/components/ui/Heading.vue";
+import { toTypedSchema } from "@vee-validate/zod";
 const title = ref("Edit Category");
 const description = ref("Edit Category");
 const toastMessage = ref("Category Updated");
 const actions = ref("Save Changes");
 const isEditing = ref(true);
+const route = useRoute();
+const { data } = await useFetch(
+  `/api/admin/categories/${route.params.categoryId}`
+);
+const { handleSubmit, errors } = useForm({
+  validationSchema: toTypedSchema(categorySchema),
+  initialValues: {},
+});
 </script>
