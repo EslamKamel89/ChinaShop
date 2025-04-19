60919a22a61a804c4baa2bd9a755e6b3aeabd8a2
EslamKamel89 <elsamkamelforex@gmail.com>
2025-04-19 11:44:33 +0200

    feat: implement modal component with props for visibility, title, and description

--- components/shared/Modal.vue
+++ components/shared/Modal.vue
@@ -1,5 +1,35 @@
-<template>
-  <div></div>
-</template>
+<script setup lang="ts">
+const props = defineProps<{
+  isModalVisible: boolean;
+  title?: string;
+  description?: string;
+}>();
+const emits = defineEmits<{
+  onClose: [];
+}>();
+const isOpen = computed({
+  get() {
+    return props.isModalVisible;
+  },
+  set(value) {
+    emits("onClose");
+  },
+});
+</script>
 
-<script setup lang="ts"></script>
+<template>
+  <Dialog :isOpen="isOpen" @update:open="(val) => emits('onClose')">
+    <DialogTrigger> Edit Profile </DialogTrigger>
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle v-if="title">{{ title }}</DialogTitle>
+        <DialogDescription v-if="description">
+          {{ description }}
+        </DialogDescription>
+      </DialogHeader>
+      <div>
+        <slot />
+      </div>
+    </DialogContent>
+  </Dialog>
+</template>
