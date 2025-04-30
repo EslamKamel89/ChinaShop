3de1faa9c0e9544eed98a4262579e81058b031e2
EslamKamel89 <elsamkamelforex@gmail.com>
2025-04-30 10:46:01 +0300

    fix: update query parameter handling and improve filter logic in product retrieval

--- server/api/admin/products/index.get.ts
+++ server/api/admin/products/index.get.ts
@@ -1,14 +1,22 @@
 import { ProductFilters } from "~/types";
 import db from "~/utils/db";
+import pr from "~/utils/pr";
 
 export default defineEventHandler(async (event) => {
+  // const { colorId, sizeId, categoryId, isArchived, isFeatured } =
+  //   getQuery<ProductFilters>(event);
+  const url = getRequestURL(event);
   const { colorId, sizeId, categoryId, isArchived, isFeatured } =
-    getQuery<ProductFilters>(event);
+    Object.fromEntries(url.searchParams.entries()) as ProductFilters;
   const filters = () => {
     let result = {};
     if (colorId) result = { ...result, colorId };
     if (sizeId) result = { ...result, sizeId };
     if (categoryId) result = { ...result, categoryId };
+    if (isArchived) result = { ...result, isArchived: !!isArchived };
+    if (isFeatured) result = { ...result, isFeatured: !!isFeatured };
+    pr(result, "query");
+    pr(getQuery(event), "query raw");
     return result;
   };
   const products = await db.product.findMany({
